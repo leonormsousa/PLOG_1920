b3babf3f7191cc9aa378bd0dbcf5984a3574dfa8
SilviaRocha <silviajmr99@gmail.com>
2019-11-13 10:55:31 +0000

    Added Menu

--- PLOG_TP1_RI_Iris_4.pl
+++ PLOG_TP1_RI_Iris_4.pl
@@ -1,3 +1,27 @@
+initialBoard([[1,2,1,1,2,1,2,2],[2,1,2,2,1,2,1,2,1], [2,1,1,2,2,1,2,2,1,2], [1,2,1,1,2,1,1,2,1,2,1], [1,2,2,2,2,2,2,2,1,2,1,1],  [2,1,2,2,1,1,1,2,1,1,1,2,1], [1,2,1,2,1,2,1,1,1,2,1,2,2,1], [2,1,2,1,1,2,1,1,2,2,1,2,1,2,2], [1,2,2,1,1,2,1,1,1,2,2,1,2,2],[2,1,1,2,1,2,1,2,2,1,1,2,1], [2,1,2,2,2,2,1,2,2,1,1,2], [1,1,2,1,2,1,1,1,2,2,2], [1,2,2,1,1,2,2,2,1,1], [1,2,2,1,1,2,2,1,2],[2,2,1,2,1,1,2,1]]).
+
+displayMenu :-
+    write('|                                                       |\n'),
+    write('|                  ___  __   ___   ___                  |\n'),
+    write('|                   |  |  |   |   |                     |\n'),
+    write('|                   |  | _|   |   |__                   |\n'),
+    write('|                   |  |\\     |      |                  |\n'),
+    write('|                  _|_ | \\_  _|_  ___|                  |\n'),
+    write('|                                                       |\n'),
+    write('|                                                       |\n'),
+    write('|              ___________________________              |\n'),
+    write('|              |                         |              |\n'),
+    write('|              |   1. Player vs Player   |              |\n'),
+    write('|              |_________________________|              |\n'),
+    write('|              |                         |              |\n'),
+    write('|              |  2. Player vs Computer  |              |\n'),
+    write('|              |_________________________|              |\n'),
+    write('|              |                         |              |\n'),
+    write('|              | 3. Computer vs Computer |              |\n'),
+    write('|              |_________________________|              |\n'),
+    write('|                                                       |\n'),
+    write('|                                                       |\n').
+   
 piece(1) :- write('1 ').
 piece(b) :- write('B ').
 piece(2) :- write('2 ').
@@ -20,7 +44,8 @@ drawLine([], 1) :- write('\\ \n').
 drawLine([], 2) :- write('| \n').
 drawLine([], 3) :- write('/ \n').
 drawLine([H | T], N) :- piece(H), drawLine(T, N).
+
+parseInput(1, Board) :- !, displayBoard(Board, 1).
+
 displayBoard([H|T], N):- drawSeparator(N), N1 is N+1, (N1 == 9 -> drawLine(H,2);(N1 >= 9 -> drawLine(H,3); drawLine(H,1))) , displayBoard(T, N1).
-display_game(Board, Player) :- displayBoard(Board, 1).
-
-
+display_game(Board, Player) :- displayMenu, write('--> Insert your option: '), read(Input), \+parseInput(Input, Board).
